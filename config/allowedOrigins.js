096da04c4d64f18ab79c5a5b2666ecb640bf297c
dansbot <danrparis@gmail.com>
2023-03-25 19:21:48 -0700

    updating cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -4,6 +4,7 @@ const allowedOrigins = [
   "http://127.0.0.1:5500",
   "http://localhost:5500",
   "http://localhost:3000",
+  "https://easyai.herokuapp.com",
 ];
 
 module.exports = allowedOrigins;
